9be64320fee7331235da515eaac64092c28335cf
FerreiraGustavo7 <gferreira.7391@gmail.com>
2026-04-01 13:47:22 -0300

    Conversor de moedas

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function idadeatual() {
-  var anonascimento = Number(
-    window.document.getElementById("txtanoascimento").value,
-  );
-  var anoatual = Number(window.document.getElementById("txtanoatual").value);
-  var idade = window.document.getElementById("txtidade");
-  var idade = anoatual - anonascimento;
-
-  txtidadeatual.value = idade;
+function converter() {
+  var valor_reais = document.getElementById("txtvalor_reais").value;
+  var valor_convertido = valor_reais / 5;
+  document.getElementById("txtvalor_convertido").value = valor_convertido;
 }
